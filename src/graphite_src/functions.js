c876d3590580e6c962a06a21b0ed9e6c2a3091ed
Gaurav Bajaj <goru97@gmail.com>
2016-05-23 00:21:09 -0700

    Fixing Repose

---
+++ src/graphite_src/functions.js
@@ -0,0 +1,23 @@
+class functions{
+    constructor(){
+       super();
+    }
+    not_empty(values) {
+        values.forEach(v =>{
+        if (v !== null){
+            return True
+        }
+        return False
+        });
+    }
+
+
+   /* safe(f) {
+        var inner = function (values){
+            vals = list(not_none(values))
+            if not vals
+            return
+        return f(vals)
+        return inner
+    }*/
+}
